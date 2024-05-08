1ae194bdf49cc94fb74e0bacf0decbdc6b33de45
Jose Jurado Cortés <jjuracor@myuax.com>
2024-05-08 10:32:08 +0200

    .

--- src/main/java/tweet/Retweet.java
+++ src/main/java/tweet/Retweet.java
@@ -1,4 +1,22 @@
 package tweet;
 
-public class Retweet {
+import user.UserAccount;
+import java.time.LocalDate;
+
+public class Retweet extends Tweet {
+    private Tweet retweetedTweet;
+
+    public Retweet(UserAccount sender, String message, LocalDate time, Tweet retweetedTweet) {
+        super(sender, message, time);
+        this.retweetedTweet = retweetedTweet;
+    }
+
+    public Tweet getRetweetedTweet() {
+        return retweetedTweet;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Retweeted: " + retweetedTweet;
+    }
 }
--- src/main/java/tweet/Tweet.java
+++ src/main/java/tweet/Tweet.java
@@ -1,4 +1,36 @@
 package tweet;
 
+import user.UserAccount;
+import java.time.LocalDate;
+
 public class Tweet {
+    private UserAccount sender;
+    private String message;
+    private LocalDate time;
+
+    public Tweet(UserAccount sender, String message, LocalDate time) {
+        if (message.length() > 140) {
+            throw new IllegalArgumentException("Message cannot be longer than 140 characters");
+        }
+        this.sender = sender;
+        this.message = message;
+        this.time = time;
+    }
+
+    public UserAccount getSender() {
+        return sender;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public LocalDate getTime() {
+        return time;
+    }
+
+    @Override
+    public String toString() {
+        return "Sender: " + sender + ", Message: " + message + ", Time: " + time;
+    }
 }

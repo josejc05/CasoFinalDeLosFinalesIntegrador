13b756f2b428708fd26aad510a73618372e963a5
Jose Jurado Cortés <jjuracor@myuax.com>
2024-05-08 10:08:41 +0200

    .

---
+++ src/main/java/ejercicio4.java
@@ -0,0 +1,9 @@
+public class ejercicio4 {
+    public static int sumDigits(int num) {
+        if (num == 0) {
+            return 0;
+        } else {
+            return num % 10 + sumDigits(num / 10);
+        }
+    }
+}

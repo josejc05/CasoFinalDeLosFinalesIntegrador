4de15e79cf6f4019f9020026250906d8b52188af
Jose Jurado Cortés <jjuracor@myuax.com>
2024-05-08 10:32:30 +0200

    .

--- src/main/java/user/UserAccount.java
+++ src/main/java/user/UserAccount.java
@@ -1,4 +1,55 @@
 package user;
 
+import tweet.Tweet;
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserAccount {
+    private String alias;
+    private String email;
+    private List<Tweet> tweets;
+    private List<Tweet> timeline;
+    private List<UserAccount> followers;
+    private List<UserAccount> following;
+
+    public UserAccount(String alias, String email) {
+        if (!email.contains("@")) {
+            throw new IllegalArgumentException("Invalid email");
+        }
+        this.alias = alias;
+        this.email = email;
+        this.tweets = new ArrayList<>();
+        this.timeline = new ArrayList<>();
+        this.followers = new ArrayList<>();
+        this.following = new ArrayList<>();
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public List<Tweet> getTweets() {
+        return tweets;
+    }
+
+    public List<Tweet> getTimeline() {
+        return timeline;
+    }
+
+    public List<UserAccount> getFollowers() {
+        return followers;
+    }
+
+    public List<UserAccount> getFollowing() {
+        return following;
+    }
+
+    @Override
+    public String toString() {
+        return "Alias: " + alias + ", Email: " + email;
+    }
 }

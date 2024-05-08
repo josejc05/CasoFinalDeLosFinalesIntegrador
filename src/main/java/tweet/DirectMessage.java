6afebce4dace3494192dc9d5d89c66188255caa7
Jose Jurado Cortés <jjuracor@myuax.com>
2024-05-08 10:30:42 +0200

    .

--- src/main/java/tweet/DirectMessage.java
+++ src/main/java/tweet/DirectMessage.java
@@ -1,4 +1,22 @@
 package tweet;
 
-public class DirectMessage {
+import user.UserAccount;
+import java.time.LocalDate;
+
+public class DirectMessage extends Tweet {
+    private UserAccount receiver;
+
+    public DirectMessage(UserAccount sender, String message, LocalDate time, UserAccount receiver) {
+        super(sender, message, time);
+        this.receiver = receiver;
+    }
+
+    public UserAccount getReceiver() {
+        return receiver;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Receiver: " + receiver;
+    }
 }
